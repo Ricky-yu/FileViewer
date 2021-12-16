96d6c08980be9fd1abf54c08372099b33cfe1fbc
Ricky-yu <ts00484515@gmail.com>
2021-12-16 10:04:28 +0900

    fix code

--- FileViewer/FileViewer/Controller/ViewController.swift
+++ FileViewer/FileViewer/Controller/ViewController.swift
@@ -20,7 +20,6 @@ class ViewController: UICollectionViewController {
             self.filesUrl = fileContents
             print(fileContents)
             subfilesUrl = fileContents.filter { url in
-                //`url`がファイルかフォルダか判定して、フォルダなら`true`を返す
                 var isDirectory: ObjCBool = false
                 return FileManager.default.fileExists(atPath: url.path, isDirectory: &isDirectory) && isDirectory.boolValue
             }
@@ -39,8 +38,7 @@ extension ViewController {
    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
             let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "FileViewCell", for: indexPath) as! FileViewCell
             let item = self.filesUrl[indexPath.row]
-    cell.setupCell(title: "\(item)", image: UIImage(named: "folder")!)
-            //cell.textLabel!.text = "\(📁)  \(item.name)"
+            cell.setupCell(title: "\(item)", image: UIImage(named: "folder")!)
             return cell
         }
 }
