c50d8df811a4de58a5b86df0f75844d20392c269
Ricky-yu <ts00484515@gmail.com>
2021-12-18 02:39:55 +0900

    add Item Class

---
+++ FileViewer/FileViewer/Model/Item.swift
@@ -0,0 +1,43 @@
+//
+//  Item.swift
+//  FileViewer
+//
+//  Created by chensinyu on 2021/12/17.
+//
+
+import UIKit
+enum Datakey: CodingKey { case folder, txt}
+class Item {
+    let url: URL
+    private(set) var name: String
+    private(set) var key: Datakey
+    weak var store: Store?
+    weak var parent: Folder? {
+        didSet {
+            store = parent?.store
+        }
+    }
+    init(name: String, url: URL, key: Datakey) {
+        self.name = name
+        self.key = key
+        self.url = url
+        self.store = nil
+    }
+    func setName(_ newName: String) {
+        
+    }
+    
+    func deleted() {
+        parent = nil
+    }
+}
+
+extension Item {
+    static let changeReasonKey = "reason"
+    static let newValueKey = "newValue"
+    static let oldValueKey = "oldValue"
+    static let parentFolderKey = "parentFolder"
+    static let renamed = "renamed"
+    static let added = "added"
+    static let removed = "removed"
+}
