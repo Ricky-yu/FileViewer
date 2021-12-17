e40b8f52d77d4d096588ba4f061f032d615d08c4
Ricky-yu <ts00484515@gmail.com>
2021-12-18 02:40:05 +0900

    fix Store

--- FileViewer/FileViewer/Model/Store.swift
+++ FileViewer/FileViewer/Model/Store.swift
@@ -10,48 +10,32 @@ import UIKit
 final class Store {
     let fileManager = FileManager.default
     static let changedNotification = Notification.Name("StoreChanged")
-    static private let folderURL: URL = Bundle.main.bundleURL.appendingPathComponent(.storeLocation)
-    
+    static private let folderURL: URL = Bundle.main.bundleURL.appendingPathComponent("AFolder")
     static let shared = Store(url: folderURL)
-    
     let baseURL: URL?
     var placeholder: URL?
     let rootFolder = Folder(name: "AFolder", url: folderURL, key: .folder)
     
     init(url: URL?) {
-
         self.baseURL = url
         self.placeholder = nil
-        print(url)
         if let fileContents = try? fileManager.contentsOfDirectory(at: url!, includingPropertiesForKeys: [URLResourceKey.nameKey, URLResourceKey.isDirectoryKey], options: .skipsHiddenFiles) {
-            //self.filesUrl = fileContents
-            print(fileContents)
-//            subfilesUrl = fileContents.filter { url in
-//                var isDirectory: ObjCBool = false
-//                return FileManager.default.fileExists(atPath: url.path, isDirectory: &isDirectory) && isDirectory.boolValue
-//            }
-        } else {
             
+            for (i, url) in fileContents.enumerated() {
+                let fileName = url.absoluteString.split(separator: "/").last!
+                var isDirectory:ObjCBool = false
+                let isExist = fileManager.fileExists(atPath: url.path, isDirectory: &isDirectory)
+                if isExist && isDirectory.boolValue {
+                    rootFolder.add(Item(name: "\(fileName.removingPercentEncoding!)", url: url, key: .folder))
+                } else {
+                    rootFolder.add(Item(name: "\(fileName.removingPercentEncoding!)", url: url, key: .txt))
+                }
+            }
         }
-        self.rootFolder = Folder(name: "", uuid: UUID())
-        self.rootFolder.store = self
+            self.rootFolder.store = self
     }
     
     func save(_ notifying: Item, userInfo: [AnyHashable: Any]) {
-        if let url = baseURL, let data = try? JSONEncoder().encode(rootFolder) {
-            try! data.write(to: url.appendingPathComponent(.storeLocation))
-
-        }
         NotificationCenter.default.post(name: Store.changedNotification, object: notifying, userInfo: userInfo)
     }
-    
-    func item(atUUIDPath path: [UUID]) -> Item? {
-        return rootFolder.item(atUUIDPath: path[0...])
-    }
-    
 }
-
-fileprivate extension String {
-    static let storeLocation = "AFolder"
-}
-
