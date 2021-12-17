3c6f596b41e18c70bb65dc13f897f8ca75e09305
Ricky-yu <ts00484515@gmail.com>
2021-12-18 03:05:11 +0900

    add AlertVC for add Folder

--- FileViewer/FileViewer/Util/ExtensionViewController.swift
+++ FileViewer/FileViewer/Util/ExtensionViewController.swift
@@ -7,6 +7,21 @@
 
 import UIKit
 
-class ExtensionViewController: NSObject {
+extension UIViewController {
+    func modalTextAlert(title: String, accept: String = .ok, cancel: String = .cancel, placeholder: String, callback: @escaping (String?) -> ()) {
+        let alert = UIAlertController(title: title, message: nil, preferredStyle: .alert)
+        alert.addTextField { $0.placeholder = placeholder }
+        alert.addAction(UIAlertAction(title: cancel, style: .cancel) { _ in
+            callback(nil)
+        })
+        alert.addAction(UIAlertAction(title: accept, style: .default) { _ in
+            callback(alert.textFields?.first?.text)
+        })
+        present(alert, animated: true)
+    }
+}
 
+fileprivate extension String {
+    static let ok = NSLocalizedString("OK", comment: "")
+    static let cancel = NSLocalizedString("Cancel", comment: "")
 }
