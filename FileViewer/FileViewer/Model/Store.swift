414c1bf0d7ea0f6ef7efc79fb2bda1d299213d79
Ricky-yu <ts00484515@gmail.com>
2021-12-19 03:42:49 +0900

    fix code

--- FileViewer/FileViewer/Model/Store.swift
+++ FileViewer/FileViewer/Model/Store.swift
@@ -12,23 +12,22 @@ final class Store {
     static let changedNotification = Notification.Name("StoreChanged")
     static private let folderURL: URL = Bundle.main.bundleURL.appendingPathComponent("AFolder")
     static let shared = Store(url: folderURL)
-    let baseURL: URL?
+    var baseURL: URL?
     var placeholder: URL?
     let rootFolder = Folder(name: "AFolder", url: folderURL, key: .folder)
     
     init(url: URL?) {
         self.baseURL = url
-        self.placeholder = nil
-        if let fileContents = try? fileManager.contentsOfDirectory(at: url!, includingPropertiesForKeys: [URLResourceKey.nameKey, URLResourceKey.isDirectoryKey], options: .skipsHiddenFiles) {
+        if let fileContents = try? fileManager.contentsOfDirectory(at: self.baseURL!, includingPropertiesForKeys: [URLResourceKey.nameKey, URLResourceKey.isDirectoryKey], options: .skipsHiddenFiles) {
             
             for (i, url) in fileContents.enumerated() {
                 let fileName = url.absoluteString.split(separator: "/").last!
                 var isDirectory:ObjCBool = false
                 let isExist = fileManager.fileExists(atPath: url.path, isDirectory: &isDirectory)
                 if isExist && isDirectory.boolValue {
-                    rootFolder.add(Item(name: "\(fileName.removingPercentEncoding!)", url: url, key: .folder))
+                    rootFolder.setItem(Folder(name: "\(fileName.removingPercentEncoding!)", url: url, key: .folder))
                 } else {
-                    rootFolder.add(Item(name: "\(fileName.removingPercentEncoding!)", url: url, key: .txt))
+                    rootFolder.setItem(Folder(name: "\(fileName.removingPercentEncoding!)", url: url, key: .txt))
                 }
             }
         }
@@ -54,6 +53,6 @@ final class Store {
     }
     
     func save(_ notifying: Item, userInfo: [AnyHashable: Any]) {
-        NotificationCenter.default.post(name: Store.changedNotification, object: notifying, userInfo: userInfo)
+        NotificationCenter.default.post(name: Store.changedNotification, object: nil, userInfo: nil)
     }
 }
