a5aaf3babfed5123b60f2c3fb494e6f0949bc445
Ricky-yu <ts00484515@gmail.com>
2021-12-19 22:42:51 +0900

    add FileViewCell

--- FileViewer/FileViewer/View/FileViewCell.swift
+++ FileViewer/FileViewer/View/FileViewCell.swift
@@ -2,7 +2,7 @@
 //  FileViewCell.swift
 //  FileViewer
 //
-//  Created by chensinyu on 2021/12/16.
+//  Created by chensinyu on 2021/12/19.
 //
 
 import UIKit
@@ -10,13 +10,11 @@ import UIKit
 class FileViewCell: UICollectionViewCell {
     
     @IBOutlet weak var fileImageView: UIImageView!
-    @IBOutlet weak var fileTitle: UILabel!
     
+@IBOutlet weak var fileTitle: UILabel!
     func setupCell(item: Item) {
         self.fileTitle.text = item.name
         switch item.key {
-        case .folder:
-            self.fileImageView.image = UIImage(named: "folder")
         case .txt:
             self.fileImageView.image = UIImage(named: "file")
         default:
